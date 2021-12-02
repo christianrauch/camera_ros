5a009d588946afd8392396699fe0541f955763dd
Christian Rauch <Rauch.Christian@gmx.de>
2021-12-02 01:14:41 +0000

    apply format

--- src/camera.cpp
+++ src/camera.cpp
@@ -1,9 +1,9 @@
 #include <cstdio>
 
-int main(int argc, char ** argv)
+int main(int argc, char **argv)
 {
-  (void) argc;
-  (void) argv;
+  (void)argc;
+  (void)argv;
 
   printf("hello world camera_ros package\n");
   return 0;
