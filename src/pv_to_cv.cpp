7fe6fdbafdf7d74804697b2d33095a3976a5f41b
Christian Rauch <Rauch.Christian@gmx.de>
2022-03-28 03:04:31 +0100

    move pv_to_cv to lib

---
+++ src/pv_to_cv.cpp
@@ -0,0 +1,62 @@
+#include "pv_to_cv.hpp"
+#include "types.hpp"
+
+
+libcamera::ControlValue
+pv_to_cv_int_array(const std::vector<int64_t> &values, const libcamera::ControlType &type)
+{
+  // convert to Span (Integer32, Integer64) or geometric type Rectangle, Size
+  switch (type) {
+  case libcamera::ControlTypeInteger32:
+    return {
+      libcamera::Span<const CTInteger32>(std::vector<CTInteger32>(values.begin(), values.end()))};
+  case libcamera::ControlTypeInteger64:
+    return {libcamera::Span<const CTInteger64>(values)};
+  case libcamera::ControlTypeRectangle:
+    return {libcamera::Rectangle(values[0], values[1], values[2], values[3])};
+  case libcamera::ControlTypeSize:
+    return {libcamera::Size(values[0], values[1])};
+  default:
+    return {};
+  }
+
+  return {};
+}
+
+libcamera::ControlValue
+pv_to_cv(const rclcpp::Parameter &parameter, const libcamera::ControlType &type)
+{
+  switch (parameter.get_type()) {
+  case rclcpp::ParameterType::PARAMETER_NOT_SET:
+    return {};
+  case rclcpp::ParameterType::PARAMETER_BOOL:
+    return {parameter.as_bool()};
+  case rclcpp::ParameterType::PARAMETER_INTEGER:
+    if (type == libcamera::ControlTypeInteger32)
+      return {CTInteger32(parameter.as_int())};
+    else if (type == libcamera::ControlTypeInteger64)
+      return {CTInteger64(parameter.as_int())};
+    else
+      return {};
+  case rclcpp::ParameterType::PARAMETER_DOUBLE:
+    return {CTFloat(parameter.as_double())};
+  case rclcpp::ParameterType::PARAMETER_STRING:
+    return {parameter.as_string()};
+  case rclcpp::ParameterType::PARAMETER_BYTE_ARRAY:
+    return {libcamera::Span<const CTByte>(parameter.as_byte_array())};
+  case rclcpp::ParameterType::PARAMETER_BOOL_ARRAY:
+    return {};
+  case rclcpp::ParameterType::PARAMETER_INTEGER_ARRAY:
+    return pv_to_cv_int_array(parameter.as_integer_array(), type);
+  case rclcpp::ParameterType::PARAMETER_DOUBLE_ARRAY:
+  {
+    // convert to float vector
+    return {libcamera::Span<const CTFloat>(std::vector<CTFloat>(
+      parameter.as_double_array().begin(), parameter.as_double_array().end()))};
+  }
+  case rclcpp::ParameterType::PARAMETER_STRING_ARRAY:
+    return {libcamera::Span<const CTString>(parameter.as_string_array())};
+  }
+
+  return {};
+}
