bd78e5014b43dcba155a94b30429347e93bd81ae
Christian Rauch <Rauch.Christian@gmx.de>
2023-10-29 23:56:58 +0100

    use the actual vector size in control value conversion
    Dynamic Spans have maximum extend but variable number of elements. For
    dynamic Spans with a single element, this previously caused the exception
    'std::length_error' since a vector with "maximum extend" elements was
    constructed.
    
    Fix this by ignoring the extend and using the actual number of elements.

--- src/cv_to_pv.cpp
+++ src/cv_to_pv.cpp
@@ -13,7 +13,7 @@
 
 #define CASE_CONVERT(T)           \
   case libcamera::ControlType##T: \
-    return cv_to_pv(extract_value<ControlTypeMap<libcamera::ControlType##T>::type>(value), extent);
+    return cv_to_pv(extract_value<ControlTypeMap<libcamera::ControlType##T>::type>(value));
 
 #define CASE_NONE(T)              \
   case libcamera::ControlType##T: \
@@ -75,25 +75,23 @@ cv_to_pv_scalar(const libcamera::Size &size)
 
 template<typename T>
 rclcpp::ParameterValue
-cv_to_pv(const std::vector<T> &values, const std::size_t &extent)
+cv_to_pv(const std::vector<T> &values)
 {
-  if ((values.size() > 1 && extent > 1) && (values.size() != extent))
-    throw std::runtime_error("type extent (" + std::to_string(extent) + ") and value size (" +
-                             std::to_string(values.size()) + ") cannot be larger than 1 and differ");
-
-  if (values.size() > 1)
-    return cv_to_pv_array(values);
-  else if (values.size() == 1)
-    if (!extent)
-      return cv_to_pv_scalar(values[0]);
-    else
-      return cv_to_pv_array(std::vector<T>(extent, values[0]));
-  else
+  switch (values.size()) {
+  case 0:
+    // empty array
     return rclcpp::ParameterValue();
+  case 1:
+    // single element (scalar)
+    return cv_to_pv_scalar(values[0]);
+  default:
+    // dynamic array
+    return cv_to_pv_array(values);
+  }
 }
 
 rclcpp::ParameterValue
-cv_to_pv(const libcamera::ControlValue &value, const std::size_t &extent)
+cv_to_pv(const libcamera::ControlValue &value)
 {
   switch (value.type()) {
     CASE_NONE(None)
--- src/cv_to_pv.hpp
+++ src/cv_to_pv.hpp
@@ -5,7 +5,7 @@
 
 
 rclcpp::ParameterValue
-cv_to_pv(const libcamera::ControlValue &value, const std::size_t &extent);
+cv_to_pv(const libcamera::ControlValue &value);
 
 rclcpp::ParameterType
 cv_to_pv_type(const libcamera::ControlType &type, const bool is_array);
