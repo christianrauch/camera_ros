f317bb1bd5ac14f17775553c3ac81885e5ec3b3f
Christian Rauch <Rauch.Christian@gmx.de>
2024-05-26 01:11:00 +0200

    fix Python linting issues in launch file

--- launch/camera.launch.py
+++ launch/camera.launch.py
@@ -1,32 +1,41 @@
 import launch
+
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 
 
-def generate_launch_description():
+def generate_launch_description() -> launch.LaunchDescription:
+    """
+    Generate a launch description with for the camera node and a visualiser.
+
+    Returns
+    -------
+        launch.LaunchDescription: the launch description
+
+    """
     container = ComposableNodeContainer(
-            name='camera_container',
-            namespace='',
-            package='rclcpp_components',
-            executable='component_container',
-            composable_node_descriptions=[
-                ComposableNode(
-                    package='camera_ros',
-                    plugin='camera::CameraNode',
-                    parameters=[{
-                        "camera": 0,
-                        "width": 640,
-                        "height": 480,
-                    }],
-                    extra_arguments=[{'use_intra_process_comms': True}],
-                ),
-                ComposableNode(
-                    package='image_view',
-                    plugin='image_view::ImageViewNode',
-                    remappings=[('/image', '/camera/image_raw')],
-                    extra_arguments=[{'use_intra_process_comms': True}],
-                ),
-            ],
+        name='camera_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container',
+        composable_node_descriptions=[
+            ComposableNode(
+                package='camera_ros',
+                plugin='camera::CameraNode',
+                parameters=[{
+                    "camera": 0,
+                    "width": 640,
+                    "height": 480,
+                }],
+                extra_arguments=[{'use_intra_process_comms': True}],
+            ),
+            ComposableNode(
+                package='image_view',
+                plugin='image_view::ImageViewNode',
+                remappings=[('/image', '/camera/image_raw')],
+                extra_arguments=[{'use_intra_process_comms': True}],
+            ),
+        ],
     )
 
     return launch.LaunchDescription([container])
