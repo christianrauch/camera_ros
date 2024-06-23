aa16bae567b798debaf3d53cb31d95fcb4802880
Christian Rauch <Rauch.Christian@gmx.de>
2024-06-23 20:43:12 +0200

    remove redundant ';'

--- src/cv_to_pv.hpp
+++ src/cv_to_pv.hpp
@@ -7,7 +7,7 @@
 class invalid_conversion : public std::runtime_error
 {
 public:
-  explicit invalid_conversion(const std::string &msg) : std::runtime_error(msg) {};
+  explicit invalid_conversion(const std::string &msg) : std::runtime_error(msg) {}
 };
 
 
