c6a052529ad6e7d42d631e34234aec076dd82ec0
Christian Rauch <Rauch.Christian@gmx.de>
2024-11-11 22:00:54 +0100

    check debug logs for declared and set controls

--- test/test_param.cpp
+++ test/test_param.cpp
@@ -1,4 +1,5 @@
 #include "instantiate_component.hpp"
+#include "log_client.hpp"
 #include "param_client.hpp"
 #include <gtest/gtest.h>
 #include <rclcpp/rclcpp.hpp>
@@ -10,35 +11,76 @@ protected:
   void
   SetUp() override
   {
+    rclcpp::get_logger(CAMERA_NODE_NAME).set_level(rclcpp::Logger::Level::Debug);
+
     exec = rclcpp::executors::SingleThreadedExecutor::make_shared();
 
-    // common node options
-    rclcpp::NodeOptions options;
-    options.use_intra_process_comms(true);
+    log_client = std::make_unique<LogClient>(rclcpp::NodeOptions {}.use_intra_process_comms(true), CAMERA_NODE_NAME);
 
-    camera = instantiate_component("camera::CameraNode", options);
-    client = std::make_unique<ParamClient>(options, camera.get_node_base_interface()->get_name(), exec);
+    param_client = std::make_unique<ParamClient>(rclcpp::NodeOptions {}.use_intra_process_comms(true), CAMERA_NODE_NAME, exec);
 
-    exec->add_node(camera.get_node_base_interface());
+    exec->add_node(log_client->get_node_base_interface());
   }
 
   void
   TearDown() override
   {
     exec->remove_node(camera.get_node_base_interface());
+    exec->remove_node(log_client->get_node_base_interface());
   }
+
+  void
+  spin_all()
+  {
+    // spin log nodes until all work is done
+    exec->spin_all(std::chrono::nanoseconds {0});
+  }
+
+  void
+  instantiate_camera(const std::vector<rclcpp::Parameter> &parameter_overrides)
+  {
+    camera = instantiate_component(
+      "camera::CameraNode",
+      rclcpp::NodeOptions {}
+        .use_intra_process_comms(true)
+        .arguments({"--ros-args", "--remap", "__node:=" + CAMERA_NODE_NAME})
+        .parameter_overrides(parameter_overrides));
+
+    exec->add_node(camera.get_node_base_interface());
+  }
+
+  const std::string CAMERA_NODE_NAME = "camera";
 
   rclcpp::Executor::SharedPtr exec;
   rclcpp_components::NodeInstanceWrapper camera;
-  std::unique_ptr<ParamClient> client;
+  std::unique_ptr<ParamClient> param_client;
+  std::unique_ptr<LogClient> log_client;
 };
 
-TEST_F(ParamTest, any_param)
+
+TEST_F(ParamTest, override_default)
 {
-  // test that we have at least one node paramter
-  const std::vector<std::string> parameter_list = client->list_parameters();
-  ASSERT_FALSE(parameter_list.empty());
+  instantiate_camera({});
+
+  spin_all();
+
+  // expect: declare 'AeEnable' and 'ExposureTime'
+  ASSERT_TRUE(log_client->regex_search("declare AeEnable with default true"));
+  ASSERT_TRUE(log_client->regex_search("declare ExposureTime with default (.*)"));
+
+  // expect: only 'AeEnable' is set
+  ASSERT_TRUE(log_client->regex_search("setting bool parameter AeEnable to true"));
+  ASSERT_FALSE(log_client->regex_search("setting integer parameter ExposureTime to (.*)"));
+
+  // check that camera has parameter 'AeEnable'
+  ASSERT_TRUE(param_client->has_parameter("AeEnable"));
+  // check that camera has parameter 'ExposureTime'
+  ASSERT_TRUE(param_client->has_parameter("ExposureTime"));
+
+  // parameters should still be set with their default values
+  ASSERT_FALSE(param_client->get_parameters({"AeEnable", "ExposureTime"}).empty());
 }
+
 
 int
 main(int argc, char **argv)
