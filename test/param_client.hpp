add8916ea9eb5ee693bb7a5de8c2048635bb1dd7
Christian Rauch <Rauch.Christian@gmx.de>
2024-11-19 14:18:08 +0100

    check if parameter is set

--- test/param_client.hpp
+++ test/param_client.hpp
@@ -32,6 +32,16 @@ public:
     return client.has_parameter(parameter_name);
   }
 
+  bool
+  is_set_parameter(const std::string &parameter_name)
+  {
+    const std::vector<rclcpp::Parameter> parameters =
+      client.get_parameters({parameter_name});
+    if (parameters.empty())
+      return false;
+    return parameters.front().get_type() != rclcpp::PARAMETER_NOT_SET;
+  }
+
   std::vector<rclcpp::Parameter>
   get_parameters(const std::vector<std::string> &parameter_names)
   {
